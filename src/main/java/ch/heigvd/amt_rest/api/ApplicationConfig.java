9268f2bbee467c345a58b69ab1d477f5123ba34f
Bruno B. Carvalho <bruno.britocarvalho@heig-vd.ch>
2014-12-13 19:40:35 +0100

    changing a comment

--- src/main/java/ch/heigvd/amt_rest/api/ApplicationConfig.java
+++ src/main/java/ch/heigvd/amt_rest/api/ApplicationConfig.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.amt_rest.api;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
 /**
- *
- * Generating an easy WebServlet for us
+ * Generating an easy WebServlet for us !
  */
 
 @javax.ws.rs.ApplicationPath("api")
